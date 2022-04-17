4044d66367440b409f2a090de299dd52775b8f22
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-04-16 22:50:08 -0400

    Pit Times Release

---
+++ script.js
@@ -0,0 +1,13 @@
+(function() {
+
+    code = 402
+
+    if (code == "400") {
+        return;
+    }
+
+    if (code == "402") {
+        window.location.replace("https://pittimes.netlify.app/402");
+    }
+    
+})()
