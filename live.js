6976a1e152d9cc9dfd2eda198613385d67f16579
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-04-26 22:59:22 -0400

    Release

---
+++ live.js
@@ -0,0 +1,13 @@
+(function(){
+
+    code = 501
+
+    if(Code === "500"){
+        window.location.replace("https://pittimes.netlify.app/live")
+    }
+
+    if(Code === "501"){
+        window.location.replace("https://pittimes.netlify.app/Live-Event=Offline")
+    }
+
+})()
