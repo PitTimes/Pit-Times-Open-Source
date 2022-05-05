4a174275568f76cc13560c2f1adc9bbd3eef2233
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-05-04 22:45:41 -0400

    Release

--- _api/_location.js
+++ _api/_location.js
@@ -1,3 +1,19 @@
-fetch('https://api.ipify.org?format=json')
-    .then(results => results.json())
-    .then(data => console.log(data.ip))
+(function(){
+
+    code = 501
+    
+    if(code == "500"){
+        fetch('https://api.ipify.org?format=json')
+            .then(results => results.json())
+            .then(data => console.log(data.ip))
+        return
+    }
+    
+     if(code == "501"){
+        window.location.replace("https://pittimes.netlify.app/maintenance")
+        fetch('https://api.ipify.org?format=json')
+            .then(results => results.json())
+            .then(data => console.log(data.ip))
+    }
+    
+})()
