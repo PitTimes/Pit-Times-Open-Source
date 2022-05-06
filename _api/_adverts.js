e04a379514e73ab015a83033b95d5c546021c061
Grimm <85710701+GrimmHX@users.noreply.github.com>
2022-05-05 22:37:14 -0400

    Release

---
+++ _api/_adverts.js
@@ -0,0 +1,4 @@
+
+
+var PitTimesTV = '<a href="https://pittimes.netlify.app/tv"> <img src="./assets/img/ads/PitTimesTV.jpg"></a>'
+document.getElementsByTagName('pit-times-tv')[0].innerHTML = PitTimesTV;
